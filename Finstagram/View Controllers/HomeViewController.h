98ee2325b6a67ce6c77223e88e086e32f0f9e48f
Bruke Wossenseged <brukew@fb.com>
2021-07-06 13:57:23 -0700

    Log out and Persistance functionality added

---
+++ Finstagram/View Controllers/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  Finstagram
+//
+//  Created by Bruke Wossenseged on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Finstagram/View Controllers/HomeViewController.m
@@ -0,0 +1,45 @@
+//
+//  HomeViewController.m
+//  Finstagram
+//
+//  Created by Bruke Wossenseged on 7/6/21.
+//
+
+#import "HomeViewController.h"
+#import "Parse/Parse.h"
+#import "LoginViewController.h"
+#import "AppDelegate.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)logOutUser:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+    }];
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    appDelegate.window.rootViewController = loginViewController;
+    }
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
